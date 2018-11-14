0f618525defd1388a95b2db15735b8e126e17f83
WitheredEnd <kholmstadt@comcast.net>
2018-11-14 12:58:01 -0600

    WIP Menu

--- src/states/MainMenu.js
+++ src/states/MainMenu.js
@@ -9,14 +9,38 @@ class MainMenu extends Phaser.State {
   }
 
   create () {
-    let buttonPlay, buttonSettings, buttonControls
+    let buttonPlay, buttonSettings, buttonControls, buttonExit
+    
+    buttonPlay = this.game.add.button(this.game.world.centerX - 400, this.game.world.centerY - 150, 'PlayButton', moveToTestLevel, this, 2, 2, 3, 3) // hover, idle, on click,  
+    buttonControls = this.game.add.button(this.game.world.centerX - 400, this.game.world.centerY - 50, 'ControlsButton', moveToSettings, this, 4, 4, 5, 5)
+    buttonSettings = this.game.add.button(this.game.world.centerX - 400, this.game.world.centerY + 50, 'SettingsButton', moveToSettings, this, 6, 6, 7, 7)
+    buttonExit = this.game.add.button(this.game.world.centerX - 400, this.game.world.centerY + 150, 'SettingsButton', moveToExit, this, 12, 12, 13, 13)
+ 
+    buttonPlay.smoothed = false
+    buttonPlay.scale.setTo(5, 5)
 
-    buttonPlay = this.game.add.button(this.game.world.centerX - 100, this.game.world.centerY, 'PlayButton', moveToTestLevel, this, 0, 1, 2, 3)
+    buttonControls.smoothed = false
+    buttonControls.scale.setTo(5, 5)
+
+    buttonSettings.smoothed = false
+    buttonSettings.scale.setTo(5, 5)
+
+    buttonExit.smoothed = false
+    buttonExit.scale.setTo(5, 5)
   }
+
 }
 
 function moveToTestLevel () {
   this.state.start('TestLevel')
+}
+
+function moveToSettings () {
+  // this.state.start('Settings')
+}
+
+function moveToExit () {
+  // Exit Game
 }
 
 export default MainMenu
