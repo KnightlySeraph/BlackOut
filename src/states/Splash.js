acfd867750e12d4958ad99d7d6925ce584785258
Jonah Schmisek <Schmisekj0423@my.uwstout.edu>
2018-11-26 13:15:57 -0600

    Adding the tiled level colliders

--- src/states/Splash.js
+++ src/states/Splash.js
@@ -41,7 +41,8 @@ class Splash extends Phaser.State {
     this.load.setPreloadSprite(this.loaderBar)
 
     // Tiled Map
-    this.load.tilemap('Mytilemap', 'assets/images/testlevel_02.json', null, Phaser.Tilemap.TILED_JSON)
+    // this.load.tilemap('Mytilemap', 'assets/images/testlevel_02.json', null, Phaser.Tilemap.TILED_JSON)
+    this.load.tilemap('Mytilemap', 'assets/images/testlevel_03.json', null, Phaser.Tilemap.TILED_JSON)
     this.load.image('tiles1', 'assets/images/tiles1.png')
     this.load.image('tiles2', 'assets/images/tiles2.png')
     this.load.image('tiles2_t', 'assets/images/tiles2_transparent.png')
--- src/states/TestLevel.js
+++ src/states/TestLevel.js
@@ -44,18 +44,36 @@ class TestLevel extends Phaser.State {
   }
 
   create () {
-    // // Imports level
+    // // // Imports level
     // this.map = this.game.add.tilemap('Mytilemap')
     // this.map.addTilesetImage('tiles1', 'tiles1')
     // // this.map.addTilesetImage('tiles2', 'tiles2')
 
-    // // Creates Layers
+    // // // Creates Layers
     // this.layer3 = this.map.createLayer('bg_black')
     // this.layer2 = this.map.createLayer('bg_close')
     // this.layer1 = this.map.createLayer('bg_decor')
     // this.layer0 = this.map.createLayer('main_level')
 
-    // // Creates colliders for the level
+    // // // Creates colliders for the level
+    // // let customCollider = this.map.objects['collision']
+    // // customCollider.forEach(element => {
+    // //   if (element.rectangle) {
+    // //     this.Collider = this.game.add.sprite(element.x, element.y)
+    // //     this.game.physics.p2.enable(this.Collider)
+    // //     this.Collider.body.debug = __DEV__
+    // //     this.Collider.body.addRectangle(element.width, element.height)
+    // //     this.Collider.body.static = true
+    // //   } else if (element.polygon) {
+    // //     this.Collider = this.game.add.sprite(element.x, element.y)
+    // //     this.game.physics.p2.enable(this.Collider)
+    // //     this.Collider.body.debug = __DEV__
+    // //     this.Collider.body.addPolygon({}, element.polygon)
+    // //     this.Collider.body.static = true
+    // //   }
+    // // })
+
+    // Main collider
     // let customCollider = this.map.objects['collision']
     // customCollider.forEach(element => {
     //   this.Collider = this.game.add.sprite(element.x, element.y)
@@ -63,9 +81,10 @@ class TestLevel extends Phaser.State {
     //   this.Collider.body.debug = __DEV__
     //   this.Collider.body.addPolygon({}, element.polygon)
     //   this.Collider.body.static = true
+    //   this.Collider.body.setCollisionGroup(this.game.platformGroup)
     // })
 
-    // // REsize the world to the layers
+    // // // Resize the world to the layers
     // this.layer0.resizeWorld()
     // this.layer1.resizeWorld()
     // this.layer2.resizeWorld()
