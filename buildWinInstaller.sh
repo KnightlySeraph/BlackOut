0609aae7b7db91b31d9609bc94c1112057d80c3d
Seth Berrier <berriers@uwstout.edu>
2018-12-23 12:26:56 -0600

    Tweek to Windows Installer Script

--- buildWinInstaller.sh
+++ buildWinInstaller.sh
@@ -8,8 +8,11 @@ pkgName=BlackOut
 cd "./packaged/${dirName}/"
 mv win64 "${pkgName}"
 
+# Delete previous archive
+test "../${pkgName}-win64.zip" & rm "../${pkgName}-win64.zip"
+
 # Create archive
-zip -r "../${pkgName}-win64.zip" "./${pkgName}"
+zip -r9 "../${pkgName}-win64.zip" "./${pkgName}"
 
 # Name back and move back
 mv "${pkgName}" win64
